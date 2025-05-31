cdf444ea72f67f9f2e39faf32252587f7892985e
Ioannis Michailidis <michailidis.k.ioannis@gmail.com>
2025-05-31 21:26:38 +0200

    feat(add-upload-func): add the image upload functionality

--- client/server.js
+++ client/server.js
@@ -1,17 +1,23 @@
-import path from 'path';
-import express from 'express';
-
+import path from "path";
+import express from "express";
+import uploadRoutes from "./routes/uploadRoutes.js";
 const port = process.env.PORT || 3000;
 const app = express();
 
-const __dirname = path.resolve();
+const __dirname = path.resolve(); // set __dirname to current directory
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/frontend/build')));
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+// Make /loads directory static
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Upload Image Route
+app.use("/api/upload", uploadRoutes); // api/upload is the prefix for whatever is inside the uploadRoutes
 
 // Catch-all handler: return React's index.html for any non-API route
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Frontend server running on port ${port}`));
